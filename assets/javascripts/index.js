b38094bc21e6723745b00aac6c5a71ebede8d09c
Shuo Chen <shuo.chen@epigenesys.org.uk>
2024-04-06 16:07:00 +0100

    apply background on banner instead of body on iphone

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -18,7 +18,7 @@ const updateScroll = (force = false) => {
 
 document.addEventListener('DOMContentLoaded', () => {
   if (/iPhone|iPad|iPod/i.test(navigator.userAgent) || (navigator.userAgent.match(/Mac/) && navigator.maxTouchPoints && navigator.maxTouchPoints > 2)) {
-    $.backstretch($('#banner').data('bg-url'));
+    $('#banner').backstretch($('#banner').data('bg-url'));
   }
 
   updateScroll(true);
