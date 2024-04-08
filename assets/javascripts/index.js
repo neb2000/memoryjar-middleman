7c53a00eefc1f6689e5d0c27ade87d3945a07015
Shuo Chen <shuo.chen@epigenesys.org.uk>
2024-04-08 10:38:34 +0100

    updated animation

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,15 +1,16 @@
 require.context('../images', true);
 
 const updateScroll = (force = false) => {
-  const scrollPos = window.scrollY
+  const scrollPos = window.scrollY;
 
   if (force || scrollPos < window.innerHeight) {
-    const banner = document.querySelector('#banner')
-    const height = banner.offsetHeight / 3 * 2
-    banner.style.setProperty("--offset-top", `${scrollPos * 0.2}px`)
-    banner.style.setProperty("--banner-bg", `rgba(255,255,255,${0.3 - Math.min((scrollPos / height), 0.3)})`)
-    banner.style.setProperty("--text-color", `rgba(0,0,0,${0.75 - Math.min((scrollPos / height), 0.75)})`)
-    banner.style.setProperty("--backdrop-filter", `blur(${10 - Math.min((scrollPos / (height / 10)), 10)}px)`)
+    const banner = document.querySelector('#banner');
+    const height = banner.offsetHeight / 2;
+
+    const scrollProgression = Math.max(1 - scrollPos / height, 0);
+
+    document.body.style.setProperty("--offset-top", `${scrollPos * 0.2}px`);
+    document.body.style.setProperty('--scroll-progression', scrollProgression);
   }
 }
 
